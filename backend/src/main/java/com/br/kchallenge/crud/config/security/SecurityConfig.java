be54108160866ba60e3d26569d156db1086f440e
Felipe Vieira <luizfelipevieira01@gmail.com>
2025-06-10 15:30:02 +0000

    fix: liberação da rota do H2 para consulta ao DB via browser

--- backend/src/main/java/com/br/kchallenge/crud/config/security/SecurityConfig.java
+++ backend/src/main/java/com/br/kchallenge/crud/config/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/user").hasRole("ADMIN")
+                        .requestMatchers("/h2-console/**").permitAll() // Permitir acesso 
                         .anyRequest().authenticated())
                         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
 
